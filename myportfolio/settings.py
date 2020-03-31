b9e6d77ad6e820e5e3aed19aa71a75df63d5ff3f
Emanuel Lugo <lugo.emanuel@gmail.com>
2020-03-31 19:36:23 -0400

    various changes

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#TEMPLATES_DIR = os.path.join(BASE_DIR,'/myportfolio/templates/')
+TEMPLATES_DIR = os.path.join(BASE_DIR,'/myportfolio/templates/')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'myportfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["myportfolio/templates/"],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
